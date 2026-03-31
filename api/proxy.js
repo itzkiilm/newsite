719d3821c598db03b6e6e985a54c197e5000bdf3
itzkiilm <itzkiilm@gmail.com>
2026-03-30 19:01:07 -0500

    Delete api/proxy.js

--- api/proxy.js
+++
@@ -1,6 +0,0 @@
-import { createServer } from "@titaniumnetwork-dev/ultraviolet";
-
-export default function handler(req, res) {
-  const server = createServer();
-  server.emit("request", req, res);
-}
