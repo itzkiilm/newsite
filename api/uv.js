0b12d7f84a83d7e1bcd04f4528af59aa82593b61
itzkiilm <itzkiilm@gmail.com>
2026-03-30 19:24:49 -0500

    Update uv.js

--- api/uv.js
+++ api/uv.js
@@ -1,13 +1,14 @@
 import { createBareServer } from "@tomphttp/bare-server-node";
 
-const bare = createBareServer("/service/");
+const bare = createBareServer("/api/");
 
 export default async function handler(req, res) {
 
-if (bare.shouldRoute(req)) {
-  return bare.routeRequest(req, res);
-}
+  if (bare.shouldRoute(req)) {
+    return bare.routeRequest(req, res);
+  }
 
-res.status(404).send("Not found");
+  res.statusCode = 404;
+  res.end("Not found");
 
 }
