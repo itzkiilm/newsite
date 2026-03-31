1e142b29e5ac41965ae6f7a0f0c5f666bd45200e
itzkiilm <itzkiilm@gmail.com>
2026-03-30 19:23:43 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 prefix: "/service/",
-bare: "/api/uv/",
+bare: "/api/",
 encodeUrl: Ultraviolet.codec.xor.encode,
 decodeUrl: Ultraviolet.codec.xor.decode,
 handler: "/uv/uv.handler.js",
