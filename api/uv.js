85ec2b0e632c6fcc315b131330f82d260e242462
itzkiilm <itzkiilm@gmail.com>
2026-03-30 19:09:49 -0500

    Update uv.js

--- api/uv.js
+++ api/uv.js
@@ -1,3 +1,13 @@
+import { createBareServer } from "@tomphttp/bare-server-node";
+
+const bare = createBareServer("/service/");
+
 export default async function handler(req, res) {
-  res.status(200).send("Ultraviolet server running");
+
+if (bare.shouldRoute(req)) {
+  return bare.routeRequest(req, res);
+}
+
+res.status(404).send("Not found");
+
 }
